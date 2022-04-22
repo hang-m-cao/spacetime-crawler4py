a47b62413127cfe3921bd47a717f7d5a287912cc
Rochelle Nixon <rochellenixon02@gmail.com>
2022-04-22 19:23:10 +0000

    fixed issue with / and urljoin

--- scraper.py
+++ scraper.py
@@ -25,7 +25,6 @@ def extract_next_links(url, resp, crawler_data):
     
     if resp.status != 200 or resp.raw_response == None:
         return []
-
     parsed_url = urlparse(resp.url.strip())
     
     # get all links from html page using <a href>
@@ -38,7 +37,7 @@ def extract_next_links(url, resp, crawler_data):
     
     for link in set(soup.find_all('a')):
 
-        href = link.get('href')        
+        href = link.get('href')  
         
         # eliminate '/' and fragments
         if (not href or len(href) <= 1 or href[0] == '#'):
@@ -49,6 +48,8 @@ def extract_next_links(url, resp, crawler_data):
         
         # join to convert to absolute
         # determine if link is relative (attach current URL) or absolute (leave as is)
+        if url[-1] != '/':
+            url += '/'
         abs_link = urljoin(url, href)
         
         # remove fragments if any
@@ -82,7 +83,7 @@ def is_valid(url, crawler_data):
             return False
         
         if re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            r".*\.(css|js|bmp|gif|jpe?g|ico|img|war"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
